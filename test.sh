602f6821deb9f8ce59da66a14240f5098263496b
Yoonchulchung <yoonchul005@gmail.com>
2026-01-22 16:36:27 +0900

    fix(#0): test.sh 파일 수정

--- test.sh
+++ test.sh
@@ -4,8 +4,7 @@ echo "=== 기존 컨테이너 종료 및 삭제 ==="
 docker rm -f spot-fe 2>/dev/null || true
 
 echo "=== Spot-FE 빌드 및 실행 ==="
-cd docker compose up --build -d
-cd ..
+docker compose up --build -d
 
 echo "=== 실행 중인 컨테이너 확인 ==="
 docker ps --filter "name=spot-fe"
